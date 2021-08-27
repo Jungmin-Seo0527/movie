eb2c8af7b1846551116100dac00dc7b1a91b4c1f
Jungmin-Seo0527 <soato1405@naver.com>
2021-08-28 01:14:21 +0900

    사용하지 않는 import 제거

--- src/main/java/com/Jungmin/movie/crawling/MovieCrawling.java
+++ src/main/java/com/Jungmin/movie/crawling/MovieCrawling.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Component;
